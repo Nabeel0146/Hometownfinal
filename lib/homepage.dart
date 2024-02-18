0abe45e33c52e86bf2d20087dec34ee010e79645
Nabeel0146 <muhammednabeelhere@gmail.com>
2024-02-18 15:53:50 +0530

    error fixed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
-class homepage extends StatelessWidget {
-  const homepage({super.key});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      
+      
+    );
   }
 }
